e93a59f1911489eb7aa92b2c22ef4314a6fcd7fd
AivarasCer <aivaras1008@gmail.com>
2025-12-29 16:49:18 +0200

    Update main.py to include get_num_chars

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def get_book_text(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
@@ -7,4 +8,6 @@ def get_book_text(filepath):
 if __name__ == "__main__":
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_num_words(book_text)
+    char_count = get_num_chars(book_text)
     print(f'Found {word_count} total words')
+    print(char_count)
