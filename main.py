1ae74a08f427c07e3a1d8b9ae34b049cee176754
AivarasCer <aivaras1008@gmail.com>
2025-12-29 18:06:00 +0200

    Update to remove hardcoded values

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import sys
 from stats import (get_num_words, get_num_chars, chars_dict_to_sorted_list)     
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
+    word_count = get_num_words(book_text)
+    char_count = get_num_chars(book_text)
+    chars_sorted_list = chars_dict_to_sorted_list(char_count)
+    print_report(book_path, word_count, chars_sorted_list)
+
 
 def get_book_text(filepath):
     with open(filepath, "r", encoding="utf-8") as file:
@@ -18,8 +32,4 @@ def print_report(book_path, num_words, chars_sorted_list):
     print("============= END ===============")
 
 if __name__ == "__main__":
-    book_text = get_book_text("books/frankenstein.txt")
-    word_count = get_num_words(book_text)
-    char_count = get_num_chars(book_text)
-    chars_sorted_list = chars_dict_to_sorted_list(char_count)
-    print_report("books/frankenstein.txt", word_count, chars_sorted_list)
+    main()
