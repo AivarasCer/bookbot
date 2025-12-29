f29e15ed8589ef6c095e0d8f48df57ec65137c0d
AivarasCer <aivaras1008@gmail.com>
2025-12-29 16:49:01 +0200

    Add get_num_chars

--- stats.py
+++ stats.py
@@ -1,3 +1,18 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_chars(text):
+    char_dict = {}
+    lowercase = text.lower()
+    
+    for char in lowercase:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    
+    return char_dict
+
+
+            
